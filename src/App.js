7d8804c0b2573604c0dae85ef45131dc792175a8
sunilchaudhary3112 <sunilchaudhary3112@gmail.com>
2020-06-22 20:39:38 +0530

    added basic JobBoard component & josn data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import JobBoard from "./components/JobBoard";
+import data from "./assets/data.json";
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    setJobs(data);
+  }, [])
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      {
+        jobs.length === 0 ? (
+          <p> Loadings Jobs list</p>
+        ) : (
+            jobs.map(job => (
+              <JobBoard job={job} key={job.id}/>
+            ))
+          )
+      }
+      <JobBoard />
     </div>
   );
 }
---
+++ src/components/JobBoard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const JobBoard = ()=> {
+    return(
+        <div>Hi, Iam JobBoard</div>
+    )
+}
+
+export default JobBoard;
